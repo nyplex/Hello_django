1ed047a15a2ba9a2d7f390dfe700bb2bef31a003
Alex Nypels <nypels.alexandre@outlook.com>
2022-03-02 10:22:00 +0000

    Adding secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
-
-development = False
+if os.path.exists("env.py"):
+    import env
+    
+development = os.environ.get('DEVELOPMENT')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-f!a^76%3_cafg^sn@x=bf1loqog@fl6+iaa22^5_m_nv#=_^w-')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
